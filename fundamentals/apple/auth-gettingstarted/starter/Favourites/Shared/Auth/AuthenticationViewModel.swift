10db9ce7464493d6d7a4fc9fc33440685da76d08
Peter Friese <peter@peterfriese.de>
2022-07-08 13:53:20 +0200

    ✨ Starter project for Getting started with Firebase Auth for Apple platforms

---
+++ fundamentals/apple/auth-gettingstarted/starter/Favourites/Shared/Auth/AuthenticationViewModel.swift
@@ -0,0 +1,107 @@
+//
+//  AuthenticationViewModel.swift
+//  Favourites
+//
+//  Created by Peter Friese on 08.07.2022
+//  Copyright © 2021 Google LLC. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+
+enum AuthenticationState {
+  case unauthenticated
+  case authenticating
+  case authenticated
+}
+
+enum AuthenticationFlow {
+  case login
+  case signUp
+}
+
+@MainActor
+class AuthenticationViewModel: ObservableObject {
+  @Published var email: String = ""
+  @Published var password: String = ""
+  @Published var confirmPassword: String = ""
+
+  @Published var flow: AuthenticationFlow = .login
+
+  @Published var isValid: Bool  = false
+  @Published var authenticationState: AuthenticationState = .unauthenticated
+  @Published var errorMessage: String = ""
+  @Published var displayName: String = ""
+
+  init() {
+    registerAuthStateHandler()
+
+    $flow
+      .combineLatest($email, $password, $confirmPassword)
+      .map { flow, email, password, confirmPassword in
+        flow == .login
+          ? !(email.isEmpty || password.isEmpty)
+          : !(email.isEmpty || password.isEmpty || confirmPassword.isEmpty)
+      }
+      .assign(to: &$isValid)
+  }
+
+  func registerAuthStateHandler() {
+  }
+
+  func switchFlow() {
+    flow = flow == .login ? .signUp : .login
+    errorMessage = ""
+  }
+
+  private func wait() async {
+    do {
+      print("Wait")
+      try await Task.sleep(nanoseconds: 1_000_000_000)
+      print("Done")
+    }
+    catch { }
+  }
+
+  func reset() {
+    flow = .login
+    email = ""
+    password = ""
+    confirmPassword = ""
+  }
+}
+
+extension AuthenticationViewModel {
+  func signInWithEmailPassword() async -> Bool {
+    authenticationState = .authenticating
+    await wait()
+    authenticationState = .authenticated
+    return true
+  }
+
+  func signUpWithEmailPassword() async -> Bool {
+    authenticationState = .authenticating
+    await wait()
+    authenticationState = .authenticated
+    return true
+  }
+
+  func signOut() {
+    authenticationState = .unauthenticated
+  }
+
+  func deleteAccount() async -> Bool {
+    authenticationState = .unauthenticated
+    return true
+  }
+}
