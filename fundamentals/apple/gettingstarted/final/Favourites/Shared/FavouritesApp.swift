8965cd0af9ae25370ebc0f6703889cbbedf85bc5
Peter Friese <peter@peterfriese.de>
2022-10-25 18:07:21 +0200

    🧹 Cleanup

--- fundamentals/apple/gettingstarted/final/Favourites/Shared/FavouritesApp.swift
+++ fundamentals/apple/gettingstarted/final/Favourites/Shared/FavouritesApp.swift
@@ -25,7 +25,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
-//    Firebase.Analytics.setAnalyticsCollectionEnabled(true)
+    Firebase.Analytics.setAnalyticsCollectionEnabled(true)
     return true
   }
 }
