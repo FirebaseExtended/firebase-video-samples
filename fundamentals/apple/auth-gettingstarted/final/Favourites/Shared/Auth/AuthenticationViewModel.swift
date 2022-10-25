675583760be35416ac0aae427184d8b85fa95d7f
Peter Friese <peter@peterfriese.de>
2022-10-25 16:40:22 +0200

    Add source code for Getting started with Firebase Auth for Apple platforms (#8)
    * ✨ Add starter project for Getting started with Firebase Auth for Apple platforms
    * ✨ Add final state for Getting started with Firebase Auth for Apple platforms

---
+++ fundamentals/apple/auth-gettingstarted/final/Favourites/Shared/Auth/AuthenticationViewModel.swift
@@ -0,0 +1,148 @@
+//
+//  AuthenticationViewModel.swift
+//  Favourites
+//
+//  Created by Peter Friese on 08.07.2022
+//  Copyright © 2021 Google LLC. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+import FirebaseAuth
+
+enum AuthenticationState {
+  case unauthenticated
+  case authenticating
+  case authenticated
+}
+
+enum AuthenticationFlow {
+  case login
+  case signUp
+}
+
+@MainActor
+class AuthenticationViewModel: ObservableObject {
+  @Published var email = ""
+  @Published var password = ""
+  @Published var confirmPassword = ""
+
+  @Published var flow: AuthenticationFlow = .login
+
+  @Published var isValid  = false
+  @Published var authenticationState: AuthenticationState = .unauthenticated
+  @Published var errorMessage = ""
+  @Published var user: User?
+  @Published var displayName = ""
+
+  init() {
+    registerAuthStateHandler()
+
+    $flow
+      .combineLatest($email, $password, $confirmPassword)
+      .map { flow, email, password, confirmPassword in
+        flow == .login
+          ? !(email.isEmpty || password.isEmpty)
+          : !(email.isEmpty || password.isEmpty || confirmPassword.isEmpty)
+      }
+      .assign(to: &$isValid)
+  }
+
+  private var authStateHandler: AuthStateDidChangeListenerHandle?
+
+  func registerAuthStateHandler() {
+    if authStateHandler == nil {
+      authStateHandler = Auth.auth().addStateDidChangeListener { auth, user in
+        self.user = user
+        self.authenticationState = user == nil ? .unauthenticated : .authenticated
+        self.displayName = user?.email ?? ""
+      }
+    }
+  }
+
+  func switchFlow() {
+    flow = flow == .login ? .signUp : .login
+    errorMessage = ""
+  }
+
+  private func wait() async {
+    do {
+      print("Wait")
+      try await Task.sleep(nanoseconds: 1_000_000_000)
+      print("Done")
+    }
+    catch {
+      print(error.localizedDescription)
+    }
+  }
+
+  func reset() {
+    flow = .login
+    email = ""
+    password = ""
+    confirmPassword = ""
+  }
+}
+
+// MARK: - Email and Password Authentication
+
+extension AuthenticationViewModel {
+  func signInWithEmailPassword() async -> Bool {
+    authenticationState = .authenticating
+    do {
+      try await Auth.auth().signIn(withEmail: self.email, password: self.password)
+      return true
+    }
+    catch  {
+      print(error)
+      errorMessage = error.localizedDescription
+      authenticationState = .unauthenticated
+      return false
+    }
+  }
+
+  func signUpWithEmailPassword() async -> Bool {
+    authenticationState = .authenticating
+    do  {
+      try await Auth.auth().createUser(withEmail: email, password: password)
+      return true
+    }
+    catch {
+      print(error)
+      errorMessage = error.localizedDescription
+      authenticationState = .unauthenticated
+      return false
+    }
+  }
+
+  func signOut() {
+    do {
+      try Auth.auth().signOut()
+    }
+    catch {
+      print(error)
+      errorMessage = error.localizedDescription
+    }
+  }
+
+  func deleteAccount() async -> Bool {
+    do {
+      try await user?.delete()
+      return true
+    }
+    catch {
+      errorMessage = error.localizedDescription
+      return false
+    }
+  }
+}
---
+++ fundamentals/apple/auth-gettingstarted/final/Favourites/Shared/FavouriteNumberView.swift
@@ -0,0 +1,77 @@
+//
+//  FavouriteNumberView.swift
+//  Favourites
+//
+//  Created by Peter Friese on 08.07.2022
+//  Copyright © 2022 Google LLC. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import SwiftUI
+import Combine
+import FirebaseAnalytics
+import FirebaseAnalyticsSwift
+
+class FavouriteNumberViewModel: ObservableObject {
+  @Published var favouriteNumber: Int = 42
+
+  private var defaults = UserDefaults.standard
+  private let favouriteNumberKey = "favouriteNumber"
+  private var cancellables = Set<AnyCancellable>()
+
+  init() {
+    if let number = defaults.object(forKey: favouriteNumberKey) as? Int {
+      favouriteNumber = number
+    }
+    $favouriteNumber
+      .sink { number in
+        self.defaults.set(number, forKey: self.favouriteNumberKey)
+        Analytics.logEvent("stepper", parameters: ["value" : number])
+      }
+      .store(in: &cancellables)
+  }
+}
+
+struct FavouriteNumberView: View {
+  @StateObject var viewModel = FavouriteNumberViewModel()
+  var body: some View {
+    VStack {
+      Text("What's your favourite number?")
+        .font(.title)
+        .multilineTextAlignment(.center)
+      Spacer()
+      Stepper(value: $viewModel.favouriteNumber, in: 0...100) {
+        Text("\(viewModel.favouriteNumber)")
+      }
+    }
+    .frame(maxHeight: 150)
+    .foregroundColor(.white)
+    .padding()
+    #if os(iOS)
+    .background(Color(UIColor.systemPink))
+    #endif
+    .clipShape(RoundedRectangle(cornerRadius: 16))
+    .padding()
+    .shadow(radius: 8)
+    .navigationTitle("Favourite Number")
+    .analyticsScreen(name: "\(FavouriteNumberView.self)")
+  }
+}
+
+struct FavouriteNumberView_Previews: PreviewProvider {
+  static var previews: some View {
+    NavigationView {
+      FavouriteNumberView()
+    }
+  }
+}
