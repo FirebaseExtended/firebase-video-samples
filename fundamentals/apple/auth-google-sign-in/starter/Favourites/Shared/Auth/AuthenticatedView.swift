7249548f859bc7e3d5803bbd26b0f35114281f60
Peter Friese <peter@peterfriese.de>
2022-11-22 09:58:35 +0100

    📄 Fix license in headers

--- fundamentals/apple/auth-google-sign-in/starter/Favourites/Shared/Auth/AuthenticatedView.swift
+++ fundamentals/apple/auth-google-sign-in/starter/Favourites/Shared/Auth/AuthenticatedView.swift
@@ -3,7 +3,7 @@
 // Favourites
 //
 // Created by Peter Friese on 08.07.2022
-// Copyright © 2022 Google LLC. All rights reserved.
+// Copyright © 2022 Google LLC.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
