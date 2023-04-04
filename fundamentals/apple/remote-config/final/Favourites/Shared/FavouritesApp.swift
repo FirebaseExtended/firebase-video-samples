a72d5c423c9dacc54a2d0de642dd0c2ed880ffaf
Peter Friese <peter@peterfriese.de>
2023-04-04 16:51:02 +0200

    🚀 Add starter project(s) for Remote Config
    Signed-off-by: Peter Friese <peter@peterfriese.de>

---
+++ fundamentals/apple/remote-config/final/Favourites/Shared/FavouritesApp.swift
@@ -0,0 +1,44 @@
+//
+// FavouritesApp.swift
+// Shared
+//
+// Created by Peter Friese on 05.07.22.
+// Copyright © 2023 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import SwiftUI
+import Firebase
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    Analytics.setAnalyticsCollectionEnabled(true)
+    return true
+  }
+}
+
+@main
+struct FavouritesApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        FavouriteNumberView()
+      }
+    }
+  }
+}
