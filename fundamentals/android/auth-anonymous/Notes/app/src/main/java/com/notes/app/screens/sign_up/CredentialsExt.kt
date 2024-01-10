ad237dc4559a05263621cd4dfdd11f6b93fd2a3b
Marina Coelho <marinacoelho16@gmail.com>
2024-01-10 13:39:57 +0000

    Fix comment on CredentialsExt
    Co-authored-by: rachelsaunders <52258509+rachelsaunders@users.noreply.github.com>

--- fundamentals/android/auth-anonymous/Notes/app/src/main/java/com/notes/app/screens/sign_up/CredentialsExt.kt
+++ fundamentals/android/auth-anonymous/Notes/app/src/main/java/com/notes/app/screens/sign_up/CredentialsExt.kt
@@ -3,7 +3,7 @@ package com.notes.app.screens.sign_up
 import android.util.Patterns
 import java.util.regex.Pattern
 
-// Passwords should have at least six digits and include
+// Passwords must have at least six digits and include
 // one digit, one lower case letter and one upper case letter.
 private const val MIN_PASS_LENGTH = 6
 private const val PASS_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{4,}$"
