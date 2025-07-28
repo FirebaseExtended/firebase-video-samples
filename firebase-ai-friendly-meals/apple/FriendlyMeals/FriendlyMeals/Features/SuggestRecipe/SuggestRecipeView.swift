ad95715c76a2933ac2779ed93775ee9b7c74d255
Peter Friese <peter@peterfriese.de>
2025-07-28 09:46:02 +0200

    Friendly Meals for iOS (#115)
    This adds the basic version of Friendly Meals for iOS. It provides a "Suggest Recipe" screen that allows users to ask Gemini (via Firebase AI Logic) to generate a recipe based on the ingredients and any notes they provide.

---
+++ firebase-ai-friendly-meals/apple/FriendlyMeals/FriendlyMeals/Features/SuggestRecipe/SuggestRecipeView.swift
@@ -0,0 +1,84 @@
+//
+// FriendlyMeals
+//
+// Created by Peter Friese on 02.07.25.
+// Copyright © 2025 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import SwiftUI
+
+struct SuggestRecipeView: View {
+  @State private var viewModel = SuggestRecipeViewModel()
+
+  var body: some View {
+    Form {
+      Section("Ingredients") {
+        TextField(
+          "Enter ingredients",
+          text: $viewModel.ingredients,
+          axis: .vertical
+        )
+        .lineLimit(10...10)
+      }
+      Section("Notes") {
+        TextField(
+          "Any notes or preferred cuisines?",
+          text: $viewModel.notes,
+          axis: .vertical
+        )
+        .lineLimit(10...10)
+      }
+      Section {
+        Button(action: {
+          Task {
+            do {
+              try await viewModel.generateRecipe()
+            }
+            catch {
+              print(error.localizedDescription)
+            }
+          }
+        }) {
+          if viewModel.isGenerating {
+            ProgressView()
+              .progressViewStyle(CircularProgressViewStyle())
+              .padding(.horizontal)
+          } else {
+            Text("Suggest Recipe")
+              .frame(maxWidth: .infinity)
+          }
+        }
+        .disabled(viewModel.ingredients.isEmpty || viewModel.isGenerating)
+      }
+    }
+    .navigationTitle("Suggest a recipe")
+    .sheet(isPresented: $viewModel.isPresentingRecipe) {
+      NavigationStack {
+        SuggestRecipeDetailsView(recipe: viewModel.recipe)
+          .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+              Button(action: { viewModel.isPresentingRecipe.toggle() }) {
+                Label("Close", systemImage: "xmark")
+              }
+            }
+          }
+      }
+    }
+
+  }
+}
+
+#Preview {
+  SuggestRecipeView()
+}
