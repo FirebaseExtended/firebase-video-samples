40d26b5cbe735baf7b74b9e524d06ea56699e171
Jeff Huleatt <3759507+jhuleatt@users.noreply.github.com>
2025-10-14 11:35:23 -0400

    make it quick to swap between ImageLayout and ChatLayout

--- firebase-ai-friendly-meals/web/src/App.tsx
+++ firebase-ai-friendly-meals/web/src/App.tsx
@@ -1,7 +1,9 @@
 import ImageLayout from "./components/ImageLayout"
+import ChatLayout from "./components/ChatLayout"
 
 function App() {
-  return <ImageLayout />
+  // return <ImageLayout />
+  return <ChatLayout />
 }
 
 export default App
