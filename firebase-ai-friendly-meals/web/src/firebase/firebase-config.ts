acdb2e5876179a2f496b8c4109295b21e0d6bedf
Jeff Huleatt <3759507+jhuleatt@users.noreply.github.com>
2026-01-23 13:09:58 -0500

    remove Firebase config

--- firebase-ai-friendly-meals/web/src/firebase/firebase-config.ts
+++ firebase-ai-friendly-meals/web/src/firebase/firebase-config.ts
@@ -1,14 +1,11 @@
 import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyCEy8v9h4jfheqWQhDZKQbNs22pafdi6Rk",
-  authDomain: "friendly-meals-web.firebaseapp.com",
-  projectId: "friendly-meals-web",
-  storageBucket: "friendly-meals-web.firebasestorage.app",
-  messagingSenderId: "4928195112",
-  appId: "1:4928195112:web:7904adaf2aae04cbf5431d"
-};
+export const firebaseConfig = null;
+
+if (!firebaseConfig) {
+  throw new Error('No firebase config found. Set the firebaseConfig object in firebase-config.ts');
+}
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
