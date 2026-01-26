0a168f5be19c9cc135291d90faf419477ebf87cd
Jeff Huleatt <3759507+jhuleatt@users.noreply.github.com>
2026-01-26 15:25:44 -0500

    fix favicon

--- firebase-ai-friendly-meals/web/app/root.tsx
+++ firebase-ai-friendly-meals/web/app/root.tsx
@@ -16,6 +16,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
             <head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="firebase-favicon.png" />
                 <Meta />
                 <Links />
             </head>
