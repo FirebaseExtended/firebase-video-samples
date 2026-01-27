bb2e24f90a974e38c24f217f5e8dbc7586a57ca2
Jeff Huleatt <3759507+jhuleatt@users.noreply.github.com>
2026-01-27 15:12:58 -0500

    simplify auth

--- firebase-ai-friendly-meals/web/app/firebase/auth.ts
+++ firebase-ai-friendly-meals/web/app/firebase/auth.ts
@@ -4,14 +4,8 @@ import { firebaseApp } from "./firebase";
 
 export async function getUser() {
     const auth = getAuth(firebaseApp);
-
-    if (auth.currentUser) {
-        console.log("User is already signed in");
-        return auth.currentUser;
-    }
-
-    // check again for currentUser after authStateReady
     await auth.authStateReady();
+
     if (auth.currentUser) {
         console.log("User is already signed in");
         return auth.currentUser;
