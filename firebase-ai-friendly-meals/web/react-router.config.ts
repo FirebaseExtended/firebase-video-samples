94da6e6c9d491ee969dffb52e8f5569be450a89c
Morgan Chen <morganchen12@gmail.com>
2026-04-01 16:05:00 -0700

    move web sample to its own repo

--- firebase-ai-friendly-meals/web/react-router.config.ts
+++
@@ -1,6 +0,0 @@
-import type { Config } from "@react-router/dev/config";
-
-export default {
-    // SPA mode - no server-side rendering
-    ssr: false,
-} satisfies Config;
--- firebase-ai-friendly-meals/web/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import tailwindcss from "@tailwindcss/vite"
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()]
-})
